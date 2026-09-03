98e34175037411c29ccb67073db4f991f2adee42
DLWangSan <DLWangSan@users.noreply.github.com>
2026-09-03 10:13:04 +0800

    Remove machine-specific JAVA_HOME from gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,7 @@
 @echo off
 set DIRNAME=%~dp0
-if "%JAVA_HOME%"=="" set JAVA_HOME=D:\java\jdk
-"%JAVA_HOME%\bin\java.exe" -classpath "%DIRNAME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+if "%JAVA_HOME%"=="" (
+  java -classpath "%DIRNAME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+) else (
+  "%JAVA_HOME%\bin\java.exe" -classpath "%DIRNAME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+)
